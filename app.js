f626a1f0bb8afa36bb9f506c13b766f62284a731
mizunashi-sh <ohayou8080@163.com>
2020-12-09 23:57:04 +0800

    authorization added

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
+        env: 'ecnubszm-5g5y43ob65fbb01b',
         traceUser: true,
       })
     }
-
-    this.globalData = {}
+    this.globalData = {
+      openid: null,
+    }
   }
 })
