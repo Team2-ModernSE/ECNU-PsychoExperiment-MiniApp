9f73e2dd8163d5ecad5198a1373af1dc99ab4679
o0arya0o <10185102139@stu.ecnu.edu.cn>
2020-12-13 00:02:04 +0800

    add tabs in components
    我的发布/领取中的选项卡

--- pages/my/my.js
+++ pages/my/my.js
@@ -2,10 +2,71 @@
 const app = getApp()
 Page({  
   data: {
-    userinfo: null
+    userinfo: null,
+    name:"",
+    sex:['男','女'],
+    index_sex:0,
+    grade:['16级','17级','18级','19级','20级'],
+    index_grade:0,
+    age:0
   },
   onShow(){
-    const userInfo = app.globalData.userInfo
-    this.setData({userinfo:userInfo})
+    const userInfo = app.globalData.userInfo 
+    this.setData({userinfo:userInfo}) 
+  },
+  showDialogBtn: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+  /**
+  * 弹出框蒙层截断touchmove事件
+  */
+  preventTouchMove: function () {
+  },
+  /** 
+  * 隐藏模态对话框
+  */
+  hideModal: function () {
+  this.setData({
+  showModal: false
+  });
+  },
+  /**
+  * 对话框取消按钮点击事件
+  */
+  onCancel: function () {
+  this.hideModal();
+  },
+  /**
+  * 对话框确认按钮点击事件
+  */
+   onConfirm: function () {
+    wx.showToast({
+   title: '提交成功',
+   icon: 'success',
+   duration: 2000
+   })
+   this.hideModal();
+   },
+   nameInput(e){
+    this.setData({
+      name:e.detail.value
+    })
+  },
+  changeSex(e){
+    this.setData({
+      index_sex:e.detail.value
+    })
+  },
+  changeGrade(e){
+    this.setData({
+      index_grade:e.detail.value
+    })
+  },
+  ageInput(e){
+    this.setData({
+      age:e.detail.value
+    })
   }
 })
--- pages/order/released.js
+++ pages/order/released.js
@@ -5,64 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-
-
-
+    tabs:[
+      {
+        id:0,
+        value:"待领取",
+        isActive: true
+      },
+      {
+        id:1,
+        value:"进行中",
+        isActive: false
+      },{
+        id:2,
+        value:"已完成",
+        isActive: false
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+/*
+  onShow(options) {
+  // 1 获取当前的小程序的页面栈-数组 长度最大是10页面 
+    let pages = getCurrentPages();
+    // 2 数组中 索引最大的页面就是当前页面
+    let currentPage = pages[pages.length - 1];
+    // 3 获取url上的type参数
+    const { type } = currentPage.options;
+    // 4 激活选中页面标题 当 type=1 index=0 
+    this.changeTitleByIndex(type-1);
+    this.getOrders(type);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取订单列表的方法
+  async getOrders(type) {
+    const res = await request({ url: "/my/orders/all", data: { type } });
+    this.setData({
+      orders: res.orders.map(v=>({...v,create_time_cn:(new Date(v.create_time*1000).toLocaleString())}))
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 根据标题索引来激活选中 标题数组
+  changeTitleByIndex(index) {
+    // 2 修改源数组
+    let { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3 赋值到data中
+    this.setData({
+      tabs
+    })
   },
+  handleTabsItemChange(e) {
+    // 1 获取被点击的标题索引
+    const { index } = e.detail;
+    this.changeTitleByIndex(index);
+    // 2 重新发送请求 type=1 index=0
+    this.getOrders(index+1);
+  },*/
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+//标题点击事件
+  handleTabsItemChange(e){
+    //获取被点击事件的标题
+    const index=e.detail;
+    //修改源数组
+    console.log(index);
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i==index?v.isActive=true:v.isActive=false);
+    this.setData({tabs});
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
